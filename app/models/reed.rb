18ef2e34c7b76e3a35b1e58f9e7efbd060aa4f68
marcusmalarkus <marc.a.cordier@gmail.com>
2013-10-31 18:37:43 -0700

    add Babble and Reed class mockups and tested

--- app/models/reed.rb
+++ app/models/reed.rb
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
 class Reed < ActiveRecord::Base
   belongs_to :note
-=======
-class Reed
-  def initialize
-    @ip = Array.new(4) {rand(100..200)}.join('.')
-    @timestamp = Time.now
-  end
->>>>>>> add Babble and Reed class mockups and tested
 end
