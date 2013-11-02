1dcd257ad3d0f6ba5f4eb1d4686dbcce2e780b28
mwsage <mwsage@gmail.com>
2013-11-01 17:13:56 -0700

    fix bug rel to  and  methods not found

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,13 @@
 class Note < ActiveRecord::Base
   has_many :babbles
   has_many :reeds
+
+  def title
+    self.babbles.map(&:title).join("<br>--<br>")
+  end
+
+  def signature
+    self.babbles.map(&:signature).join(".....")
+  end
 end
+
